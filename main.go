550f777d1150dfd5684a15d3a078de9978e1a338
Mwai Banda <mwai.banda@cbn.org>
2023-08-28 22:08:05 -0500

    Added async fetch 100 quotes

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"fyne.io/fyne/v2"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"sync"
 	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
 
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -19,55 +22,90 @@ type Quote struct {
 	Author  string `json:"author"`
 }
 
-func getQuote(client *http.Client) Quote {
+func getQuote(client *http.Client, channel chan<- Quote, waitGroup *sync.WaitGroup) {
 	res, err := client.Get("https://api.quotable.io/quotes/random")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
+	defer waitGroup.Done()
+	body, _ := io.ReadAll(res.Body)
 	var quotes []Quote
-	decodingErr := json.Unmarshal([]byte(body), &quotes)
+	decodingErr := json.Unmarshal(body, &quotes)
 	if decodingErr != nil {
 		fmt.Println("error could not unmarshall")
 		fmt.Println(decodingErr)
 	}
 
-	return quotes[len(quotes)-1]
+	channel <- quotes[len(quotes)-1]
+}
+
+func getQuotes(client *http.Client, onQuoteAdded func(quote Quote)) {
+	startTime := time.Now()
+	quoteChannel := make(chan Quote)
+	var waitGroup sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		waitGroup.Add(1)
+		go getQuote(client, quoteChannel, &waitGroup)
+	}
+
+	go func() {
+		waitGroup.Wait()
+		close(quoteChannel)
+	}()
+
+	for quote := range quoteChannel {
+		fmt.Println(quote)
+		onQuoteAdded(quote)
+	}
+
+	fmt.Printf("Processed 100 Requests in %s", time.Since(startTime))
 }
 
 func main() {
-	client := http.Client{Timeout: time.Second + 10}
+	client := http.Client{Timeout: time.Second * 10}
 	basicTheApp := app.New()
 	window := basicTheApp.NewWindow("Random Quote")
-	window.Resize(fyne.NewSize(800, 500))
+	window.Resize(fyne.NewSize(1920, 1080))
 
-	quoteLabel := widget.NewLabel("")
-	quoteLabel.Alignment = fyne.TextAlignCenter
-	quoteLabel.Wrapping = fyne.TextWrapWord
+	var quotes []Quote
 
-	authorLabel := widget.NewLabel("")
-	authorLabel.Alignment = fyne.TextAlignCenter
-
-	quoteButton := widget.NewButton("Get Random Quote", func() {
-		quote := getQuote(&client)
-		quoteLabel.SetText(quote.Content)
-		authorLabel.SetText(quote.Author)
+	quotesList := widget.NewList(
+		func() int {
+			return len(quotes)
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabelWithStyle("template", fyne.TextAlignLeading, fyne.TextStyle{Bold: true, Symbol: true})
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(quotes[i].Content + " - " + quotes[i].Author)
+			o.(*widget.Label).Wrapping = fyne.TextTruncate
+		})
+	quoteButton := widget.NewButtonWithIcon("Get Random Quotes", theme.MediaReplayIcon(), func() {
+		quotes = []Quote{}
+		getQuotes(&client, func(quote Quote) {
+			quotes = append(quotes, quote)
+			quotesList.Refresh()
+			time.Sleep(time.Millisecond * 15)
+		})
 	})
+
 	quoteButton.Resize(fyne.NewSize(100, 100))
 
 	hStack := container.NewHBox(
-		layout.NewSpacer(),
 		quoteButton,
 		layout.NewSpacer(),
 	)
-	vStack := container.NewVBox(
-		hStack,
-		layout.NewSpacer(),
-		quoteLabel,
-		authorLabel,
-		layout.NewSpacer(),
-	)
-	window.SetContent(container.NewPadded(vStack))
-	window.ShowAndRun()
+
+	window.SetContent(container.NewPadded(container.NewBorder(hStack, nil, nil, nil, container.NewMax(quotesList))))
+	go func() {
+		window.Show()
+		getQuotes(&client, func(quote Quote) {
+			quotes = append(quotes, quote)
+			quotesList.Refresh()
+			time.Sleep(time.Millisecond * 15)
+		})
+	}()
+	basicTheApp.Run()
 }
